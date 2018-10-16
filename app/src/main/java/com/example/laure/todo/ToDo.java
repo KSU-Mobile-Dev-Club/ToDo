16f6c04b1b09efdae505bb56499a76d29f979ebe
laurel-thomson <laurel228@ksu.edu>
2018-10-16 06:43:16 -0500

    Removed Serializable interface from ToDo class

--- app/src/main/java/com/example/laure/todo/ToDo.java
+++ app/src/main/java/com/example/laure/todo/ToDo.java
@@ -6,7 +6,7 @@ import android.arch.persistence.room.PrimaryKey;
 import java.io.Serializable;
 
 @Entity(tableName = MyDatabase.TABLE_NAME_TODO)
-public class ToDo implements Serializable{
+public class ToDo{
 
     @PrimaryKey(autoGenerate = true)
     public int todo_id;
